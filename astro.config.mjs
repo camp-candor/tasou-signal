8b7f4941766a394ab0c254f4f56d0e4aad0f07d2
Brad Henderson <elliotbradly@gmail.com>
2025-10-09 09:01:12 -0400

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,14 @@ import { defineConfig } from 'astro/config'
 import cloudflare from '@astrojs/cloudflare';
 import clerk from '@clerk/astro'
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   integrations: [clerk()],
   adapter: cloudflare(),
   output: 'server',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
